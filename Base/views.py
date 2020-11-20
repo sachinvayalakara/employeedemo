707c45cf12f8499d3f17f79b0fff75a32ed04da3
sachinvayalakara <56108759+sachinvayalakara@users.noreply.github.com>
2020-11-20 16:35:27 +0530

    Update views.py

--- Base/views.py
+++ Base/views.py
@@ -33,7 +33,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 def index(request):
-    return render(request, "index.html")    
+    if 'user_id' not in request.session:
+        return HttpResponseRedirect('/login')
+    else:
+        if request.session['user_id'] == 'abc@gmail.com':
+            emp_obj = Employee.objects.all()
+            return render(request, "index.html", {'emp_obj': emp_obj, 'permission': True})
+        else:
+            emp_obj = Employee.objects.filter(email=request.session['user_id']).values('pk', 'emp_name', 'user_image', 'password', 'email', 'phone', 'address')
+            return render(request, "index.html", {'emp_obj': emp_obj, 'permission': False})
+
 
 def fn_login(request):
     try:
@@ -45,27 +54,24 @@ def fn_login(request):
                 messages.success(request, 'User not Exist')
                 return HttpResponseRedirect('/login')
             login_obj = Login.objects.filter(email=username).values('pk','email','password')
-            request.session['user_id'] = login_obj[0]['pk']
-            # print(login_obj[0]['disable_period_end'])
+            request.session['user_id'] = login_obj[0]['email']
             if login_obj[0]['password'] == password:
-
                 return HttpResponseRedirect('/index')
-
             else:
-                        
                 return redirect('/login')
                 return HttpResponseRedirect('/index')
             messages.success(request, 'Wrong password')
-        return render(request, "login.html")
+        else:
+            return render(request, "login.html")
     except Exception as e:
         print(e)
 
 
 def fn_table(request):
-    return render(request, "index.html") 
+    return render(request, "index.html")
 
 def Logout(request):
-    
+
     try:
         if request.session.has_key('user_id'):
             # del request.session['user_id']
@@ -75,8 +81,8 @@ def Logout(request):
             return HttpResponseRedirect('/login')
     except Exception as e:
         return HttpResponse(e)
-     
-         
+
+
 
 def fn_employeedetails(request):
     try:
@@ -88,29 +94,41 @@ def fn_employeedetails(request):
             address = request.POST['address']
             password = request.POST['password']
 
-            emp_obj = Employee.objects.all()
-            fs = FileSystemStorage() 
-            name = fs.save(propic.name, propic) 
-            name = 'media/' + name 
-            print(name) 
-            path = os.path.join(BASE_DIR, name) 
-            print(path) 
-            
+            fs = FileSystemStorage()
+            name = fs.save(propic.name, propic)
+            name = 'media/' + name
+            path = os.path.join(BASE_DIR, name)
+
             if email == '' or password == '' :
                 messages.success(request, 'All Fileds Are Mandatory')
 
-            
             else:
                 check_exist = Employee.objects.filter(email=email).exists()
                 if check_exist == False:
                     user = Employee(emp_name=empname, user_image=propic, email=email, phone=mobile,address=address,password=password)
                     user.save()
+
                 login_exit = Login.objects.filter(email=email).exists()
                 if login_exit == False:
-                    log = Login(email=email,password=password)
+                    log = Login(email=email, password=password)
                     log.save()
-        return render(request, 'index.html',{'emp_obj':emp_obj})
+
+        return redirect('/index')
     except Exception as e:
         print(e)
-        return HttpResponse(e) 
+        return HttpResponse(e)
+
+def Delete_data(request):
+    try:
+        if request.method == "POST":
+            idd= request.POST['id']
+            emp_mail=Employee.objects.filter(id=idd).values('email')
+            log_mail=Login.objects.filter(email=emp_mail[0]['email'])
+            log_mail.delete()
+            Employee.objects.filter(id=idd).delete()
+
+        return JsonResponse({'data':'deleted'})
     
+    except Exception as e:
+        print(e)   
+
